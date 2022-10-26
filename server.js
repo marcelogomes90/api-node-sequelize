a5976c1db92084cfd6fcafffe1bc1b7c7760eed8
Marcelo <marcelo.sobrinho@outlook.com>
2022-10-26 14:26:41 -0300

    chore: change port

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.use(routes);
 
 db.sync(() => console.log(`Banco de dados conectado: ${process.env.DB_NAME}`));
 
-app.listen(3000, () => console.log("Servidor iniciado na porta 3000"));
+app.listen(3306, () => console.log("Servidor iniciado"));
