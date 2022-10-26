4892d15c1ed1d882067444dfe9f4629167c0e2f9
Marcelo <marcelo.sobrinho@outlook.com>
2022-10-26 14:50:38 -0300

    chore: fix db port

--- src/db.js
+++ src/db.js
@@ -4,11 +4,12 @@ import dotenv from "dotenv/config.js";
 const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbHost = process.env.DB_HOST;
-const dbPassword = process.env.DB_PASSWORD; 
+const dbPassword = process.env.DB_PASSWORD;
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
     dialect: "mysql",
     host: dbHost,
+    port: 3306,
 });
 
 export default sequelize;
