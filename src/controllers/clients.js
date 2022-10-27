88dc6bad120888ce08ba6f0d0121d09967127865
marcelogomes90 <marcelo.sobrinho@outlook.com>
2022-10-26 22:01:02 -0300

    chore: clear db for new collumns

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -1,4 +1,4 @@
-import ClientRepository from "../models/clientsModel.js";
+import ClientRepository from "../models/clientsModel.js"; 
 
 function findAll(req, res) {
     ClientRepository.findAll().then((result) => res.json(result));
