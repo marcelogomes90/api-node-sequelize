f4f988f7c7e9e8e3611320d0946970c8999ad062
marcelogomes90 <marcelo.sobrinho@outlook.com>
2022-11-13 12:40:36 -0300

    chore:fix routes header

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ import clients from "./src/controllers/clients.js";
 
 const routes = express.Router();
 
-routes.use(function(req, res, next) {
+routes.all(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
