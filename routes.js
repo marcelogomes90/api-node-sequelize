450512cad6f839b40379cfbd5472804230f2960a
marcelogomes90 <marcelo.sobrinho@outlook.com>
2022-11-13 01:15:06 -0300

    chore: fix routes

--- routes.js
+++ routes.js
@@ -3,6 +3,13 @@ import clients from "./src/controllers/clients.js";
 
 const routes = express.Router();
 
+routes.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 routes.get("/clients", clients.findAll);
 routes.post("/clients", clients.addClient);
 routes.get("/clients/:id", clients.findClient);
--- server.js
+++ server.js
@@ -7,12 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(routes); 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 db.sync(() => console.log(`Banco de dados conectado: ${process.env.DB_NAME}`));
 
 app.listen(process.env.PORT || 3000, () => console.log("Servidor iniciado"));
